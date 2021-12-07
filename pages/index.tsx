9b6d241f7164b82cfd9f115bd3595449b6d3fd8e
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-12-07 10:39:01 +0500

    add Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import { Htag } from "../components"
+
 export default function Home(): JSX.Element {
-  return <div>123213fdvdfg fdgdf</div>
+  return (
+    <div>
+      <Htag tag="h1">Text</Htag>
+    </div>
+  )
 }
