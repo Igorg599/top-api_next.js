61c0a901cc2687ac5fd12a9b28050db752c992cd
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-12-08 17:29:57 +0500

    HTMLProps

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,10 @@
-import { ReactNode } from "react"
+import { ReactNode, DetailedHTMLProps, ButtonHTMLAttributes } from "react"
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
   children: ReactNode
   appearance: "primary" | "ghost"
 }
