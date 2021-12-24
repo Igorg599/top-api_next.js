a7dfa4865d3fc693230fc8e74c9b9c39fb0e6468
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-12-24 12:17:50 +0500

    add search and courses page

---
+++ pages/courses/index.tsx
@@ -0,0 +1,7 @@
+import { withLayout } from "../../layout/Layout"
+
+function Courses(): JSX.Element {
+  return <div>Courses</div>
+}
+
+export default withLayout(Courses)
---
+++ pages/search/index.tsx
@@ -0,0 +1,7 @@
+import { withLayout } from "../../layout/Layout"
+
+function Search(): JSX.Element {
+  return <div>Seacrh</div>
+}
+
+export default withLayout(Search)
