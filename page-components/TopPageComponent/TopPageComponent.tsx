71addb6c03765769376aa4c51ad7ef392fd467d0
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-01-03 13:57:29 +0500

    add title for TopPageComponent

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,22 @@
 import { TopPageComponentProps } from "./TopPageComponent.props"
+import styles from "./TopPageComponents.module.css"
+import { Htag, Tag } from "../../components"
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>
+  return (
+    <>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="grey" size="m" aria-label={products.length + "элементов"}>
+            {products.length}
+          </Tag>
+        )}
+      </div>
+    </>
+  )
 }
